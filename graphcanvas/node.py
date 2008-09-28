4078a90b6cd852f54af2cb9e78b148e2756d8f20
Rich Wareham <richwareham@gmail.com>
2008-09-28 18:24:24 +0100

    Made 'NodeItem' a subclass of Group and moved all of the node fram inot a NodeFrame object where, arguably, it should've been alla long :).

--- graphcanvas/node.py
+++ graphcanvas/node.py
@@ -9,34 +9,129 @@ import cairo
 import math
 import random
 import resizegadget
+import simple
 
-class NodeItem(tangocanvas.TangoRectItem, goocanvas.Item):
+class NodeItem(goocanvas.Group, simple.SimpleItem, goocanvas.Item):
+	def __init__(self, *args, **kwargs):
+		goocanvas.Group.__init__(self, *args, **kwargs)
+
+		self._node_data = {
+			'node-title': 'Node',
+			'x': 0.0, 'y': 0.0, 'width': 100.0, 'height': 100.0,
+			'radius-x': 0.0, 'radius-y': 0.0,
+			'color-scheme': 'Plum',
+		}
+
+		self._background_rect = NodeItemFrame( parent = self )
+
+		# make the background item draggable
+		self._dragging = False
+		self._background_rect.connect("motion_notify_event", 
+			self._on_frame_motion_notify)
+		self._background_rect.connect("button_press_event", 
+			self._on_frame_button_press)
+		self._background_rect.connect("button_release_event",
+			self._on_frame_button_release)
+
+		# Add a resize gadget item child
+		self._resize_gadget = resizegadget.ResizeGadget( parent = self,
+			orientation = resizegadget.SE)
+		self._resize_gadget.connect('button-press-event', self.foo)
+	
+	def _update_children(self):
+		propnames = ('x', 'y', 'width', 'height', 'radius-x', 
+			'radius-y', 'color-scheme')
+		for name in propnames:
+			self._background_rect.set_property(name, self._node_data[name])
+		self._background_rect.ensure_updated()
+
+		content_rect = self._background_rect.get_content_area_bounds()
+
+		# Put the resize gadget in the lower-right
+		resize_size = 15
+		self._resize_gadget.set_property('x', content_rect.x2 - resize_size - 1)
+		self._resize_gadget.set_property('y', content_rect.y2 - resize_size - 1)
+		self._resize_gadget.set_property('width', resize_size)
+		self._resize_gadget.set_property('height', resize_size)
+		c = tango.get_color_float_rgb( self._node_data['color-scheme'],
+			tango.LIGHT_CONTRAST)
+		self._resize_gadget.set_color( ( c[0], c[1], c[2], 0.5 ) )
+
+		self._resize_gadget.ensure_updated()
+
+	## event handlers
+	def _on_frame_motion_notify(self, item, target, event):
+		if((self._dragging == True) and 
+		   (event.state & gtk.gdk.BUTTON1_MASK)):
+			new_x = event.x
+			new_y = event.y
+			self.get_model().set_property('x', self._old_x + new_x - self._drag_x)
+			self.get_model().set_property('y', self._old_y + new_y - self._drag_y)
+		return True
+	
+	def _on_frame_button_press(self, item, target, event):
+		if(event.button == 1): # left button
+			self._drag_x = event.x
+			self._drag_y = event.y
+			self._old_x = self._node_data['x']
+			self._old_y = self._node_data['y']
+			self.ensure_updated()
+
+			fleur = gtk.gdk.Cursor (gtk.gdk.FLEUR)
+			canvas = item.get_canvas ()
+			canvas.pointer_grab(item,
+				gtk.gdk.POINTER_MOTION_MASK | 
+					gtk.gdk.BUTTON_RELEASE_MASK,
+				fleur, event.time)
+			self._dragging = True
+		elif event.button == 3: # right button
+			if(self.get_model() != None):
+				scheme = random.choice(tango.get_scheme_names())
+				print('Switch to: %s' % scheme)
+				self.get_model().set_property('color-scheme', scheme)
+
+		return True
+
+	def _on_frame_button_release(self, item, target, event):
+		canvas = item.get_canvas ()
+		canvas.pointer_ungrab(item, event.time)
+		self._dragging = False
+	
+	def _on_model_changed(self, model, recompute_bounds):
+		self._update_children()
+	
+	def foo(self, item, target, event):
+		print('hello: %s, %s, %s' % (item, target, event))
+	
+	## simple item methods
+	def set_model(self, model):
+		goocanvas.Group.set_model(self, model)
+
+		# so nefarious
+		self._node_data = model._node_data
+
+		self._update_children()
+
+		model.connect('changed', self._on_model_changed)
+
+gobject.type_register(NodeItem)
+
+class NodeItemFrame(tangocanvas.TangoRectItem, goocanvas.Item):
 	__gproperties__ = {
 		'node-title':	(str, None, None, 'Node',
 			gobject.PARAM_READWRITE),
 	}
 
 	def __init__(self, *args, **kwargs):
-		self._dragging = False
 		self._node_data = {
 			'node-title': 'Node',
 		}
 		self._font_size = 18
 		self._content_bounds = goocanvas.Bounds()
-		self._children = []
-
-		# HACK
-		# Flag to prevent recursion in do_simple_is_item_at
-		self._group_hack = False
 
 		# Initialise the remaining parts of the item
 		tangocanvas.TangoRectItem.__init__(self, *args, **kwargs)
-
-		# Add a resize gadget item child
-		self._resize_gadget = resizegadget.ResizeGadget( parent = self,
-			x = 100, y = 100, width = 300, height = 300,
-			orientation = resizegadget.SE)
-
+	
 	def get_node_title(self):
 		return self.get_property('node-title')
 
@@ -46,24 +141,6 @@ class NodeItem(tangocanvas.TangoRectItem, goocanvas.Item):
 	def get_content_area_bounds(self):
 		return self._content_bounds
 	
-	def _update_children(self, cr):
-		''' Internal method to update all the children. '''
-
-		content_rect = self.get_content_area_bounds()
-
-		# Put the resize gadget in the lower-right
-		resize_size = 15
-		self._resize_gadget.set_property('x',
-			content_rect.x2 - resize_size - 1)
-		self._resize_gadget.set_property('y',
-			content_rect.y2 - resize_size - 1)
-		self._resize_gadget.set_property('width', resize_size)
-		self._resize_gadget.set_property('height', resize_size)
-
-		for child in self._children:
-			child_bounds = goocanvas.Bounds()
-			child.update(True, cr, child_bounds)
-
 	## gobject methods
 	def do_get_property(self, pspec):
 		if(pspec.name == 'node-title'):
@@ -78,39 +155,6 @@ class NodeItem(tangocanvas.TangoRectItem, goocanvas.Item):
 		else:
 			tangocanvas.TangoRectItem.do_set_property(self, pspec, value)
 	
-	## group item methods
-	def do_set_canvas(self, canvas):
-		if(canvas == None):
-			return
-
-		tangocanvas.TangoRectItem.do_set_canvas(self, canvas)
-
-		for child in self._children:
-			child.set_canvas(self.get_canvas())
-
-	def do_get_n_children(self):
-		return len(self._children)
-	
-	def do_get_child(self, pos):
-		return self._children[pos]
-		
-	def do_add_child(self, child, pos):
-		if(pos == -1):
-			self._children.append(child)
-		else:
-			self._children.insert(pos, child)
-
-		child.set_parent(self)
-		if(self.get_canvas() != None):
-			child.set_canvas(self.get_canvas())
-	
-	def do_move_child(self, oldpos, newpos):
-		child = self._children.pop(oldpos)
-		self.do_add_child(child, newpos)
-		
-	def do_remove_child(self, child, pos):
-		self._children.pop(pos)
-	
 	## simple item methods
 	def set_model(self, model):
 		tangocanvas.TangoRectItem.set_model(self, model)
@@ -118,28 +162,7 @@ class NodeItem(tangocanvas.TangoRectItem, goocanvas.Item):
 		# so nefarious
 		self._node_data = model._node_data
 
-		# make this item draggable
-		self.connect("motion_notify_event", self._on_motion_notify)
-		self.connect("button_press_event", self._on_button_press)
-		self.connect("button_release_event", self._on_button_release)
-
 	def do_simple_is_item_at(self, x, y, cr, is_pointer_event):
-		if(self._group_hack):
-			return False
-
-		self._group_hack = True
-		for child in self._children:
-			child_bounds = child.get_bounds()
-			if(not boundsutils.contains_point(child_bounds, x, y)):
-				continue
-
-			items = child.get_items_at(x,y,cr,is_pointer_event,True)
-			if((items != None) and (len(items) > 0)):
-				self._group_hack = False
-				return False
-
-		self._group_hack = False
-
 		return tangocanvas.TangoRectItem.do_simple_is_item_at(self, x, y,
 			cr, is_pointer_event)
 
@@ -163,19 +186,11 @@ class NodeItem(tangocanvas.TangoRectItem, goocanvas.Item):
 			int_bounds.x1, y,
 			int_bounds.x2, int_bounds.y2)
 
-		# Recalculate the children's positions
-		self._update_children(cr)
-
 	def do_simple_create_path(self, cr):
 		tangocanvas.TangoRectItem.do_simple_create_path(self, cr)
 	
 	def do_simple_paint(self, cr, bounds):
 		tangocanvas.TangoRectItem.do_simple_paint(self, cr, bounds)
-
-		# HACK: This should be automagic
-		c = tango.get_color_float_rgb(
-			self.get_color_scheme(), tango.LIGHT_CONTRAST)
-		self._resize_gadget.set_color( ( c[0], c[1], c[2], 0.5 ) )
 
 		scheme = self.get_color_scheme()
 
@@ -218,10 +233,6 @@ class NodeItem(tangocanvas.TangoRectItem, goocanvas.Item):
 		content_bounds = boundsutils.inset( \
 			self.get_content_area_bounds(), 2.0, 2.0)
 
-		#cairoutils.rounded_rect(cr, content_bounds, 0, 0)
-		#cr.set_source_rgba(1,1,1,0.25)
-		#cr.fill()
-
 		tango.paint_pad(cr, scheme, node_bounds.x2, 
 			content_bounds.y1 + 20.5, tango.RIGHT, 15, True)
 
@@ -231,81 +242,59 @@ class NodeItem(tangocanvas.TangoRectItem, goocanvas.Item):
 		tango.paint_pad(cr, scheme, node_bounds.x2, 
 			content_bounds.y1 + 80.5, tango.RIGHT, 15)
 
-		# Draw the child elements
-		for child in self._children:
-			child.paint(cr, bounds, 1.0)
+gobject.type_register(NodeItemFrame)
 
-
-	## event handlers
-	def _on_motion_notify(self, item, target, event):
-		if((self._dragging == True) and 
-		   (event.state & gtk.gdk.BUTTON1_MASK)):
-			new_x = event.x
-			new_y = event.y
-			self.get_model().set_property('x',
-				self._old_x + new_x - self._drag_x)
-			self.get_model().set_property('y', 
-				self._old_y + new_y - self._drag_y)
-		return True
-	
-	def _on_button_press(self, item, target, event):
-		if(event.button == 1): # left button
-			self._drag_x = event.x
-			self._drag_y = event.y
-			self._old_x = self.get_property('x')
-			self._old_y = self.get_property('y')
-			self.ensure_updated()
-
-			fleur = gtk.gdk.Cursor (gtk.gdk.FLEUR)
-			canvas = item.get_canvas ()
-			canvas.pointer_grab(item,
-				gtk.gdk.POINTER_MOTION_MASK | 
-					gtk.gdk.BUTTON_RELEASE_MASK,
-				fleur, event.time)
-			self._dragging = True
-		elif event.button == 3: # right button
-			if(self.get_model() != None):
-				scheme = random.choice(tango.get_scheme_names())
-				print('Switch to: %s' % scheme)
-				self.get_model().set_property('color-scheme', scheme)
-
-			#item.lower(None)
-#		elif event.button == 3:
-			#item.raise_(None)
-		return True
-
-	def _on_button_release(self, item, target, event):
-		canvas = item.get_canvas ()
-		canvas.pointer_ungrab(item, event.time)
-		self._dragging = False
-
-gobject.type_register(NodeItem)
-
-class NodeModel(tangocanvas.TangoRectModel, goocanvas.ItemModel):
+class NodeModel(goocanvas.GroupModel, goocanvas.ItemModel):
 	__gproperties__ = {
 		'node-title':	(str, None, None, 'Node',
+			gobject.PARAM_READWRITE),
+		'x': (gobject.TYPE_DOUBLE, None, None, 
+			-gobject.G_MAXDOUBLE, gobject.G_MAXFLOAT,
+			0.0, gobject.PARAM_READWRITE),
+		'y': (gobject.TYPE_DOUBLE, None, None,
+			-gobject.G_MAXDOUBLE, gobject.G_MAXFLOAT,
+			0.0, gobject.PARAM_READWRITE),
+		'width': (gobject.TYPE_DOUBLE, None, None, 
+			0.0, gobject.G_MAXDOUBLE,
+			100.0, gobject.PARAM_READWRITE),
+		'height': (gobject.TYPE_DOUBLE, None, None,
+			0.0, gobject.G_MAXDOUBLE,
+			100.0, gobject.PARAM_READWRITE),
+		'radius-x': (gobject.TYPE_DOUBLE, None, None, 
+			0.0, gobject.G_MAXDOUBLE,
+			0.0, gobject.PARAM_READWRITE),
+		'radius-y': (gobject.TYPE_DOUBLE, None, None,
+			0.0, gobject.G_MAXDOUBLE,
+			0.0, gobject.PARAM_READWRITE),
+		'color-scheme':	(str, None, None, 'Plum',
 			gobject.PARAM_READWRITE),
 	}
 
 	def __init__(self, *args, **kwargs):
 		self._node_data = {
 			'node-title': 'Node',
+			'x': 0.0, 'y': 0.0, 'width': 100.0, 'height': 100.0,
+			'radius-x': 0.0, 'radius-y': 0.0,
+			'color-scheme': 'Plum',
 		}
 
-		tangocanvas.TangoRectModel.__init__(self, *args, **kwargs)
+		goocanvas.GroupModel.__init__(self, *args, **kwargs)
 
 	## gobject methods
 	def do_get_property(self, pspec):
-		if(pspec.name == 'node-title'):
-			return self._node_data['node-title']
+		propnames = self._node_data.keys()
+		if(pspec.name in propnames):
+			return self._node_data[pspec.name]
 		else:
-			return tangocanvas.TangoRectModel.do_get_property(self, pspec)	
+			raise AttributeError('No such property: %s' % pspec.name)
+
 	def do_set_property(self, pspec, value):
-		if(pspec.name == 'node-title'):
-			self._node_data['node-title'] = value
+		propnames = self._node_data.keys()
+		if(pspec.name in propnames):
+			self._node_data[pspec.name] = value
 			self.emit('changed', False)
 		else:
-			tangocanvas.TangoRectModel.do_set_property(self, pspec, value)
+			raise AttributeError('No such property: %s' % pspec.name)
 	
 	## item model methods
 	def do_create_item(self, canvas):
